d500e0a83604e3b701f7c6ddbbc008aa14315def
lingl <lingphoonlee@gmail.com>
2021-02-06 17:09:25 -0700

    STEP_4: SAVE_YOUR_PROGRESS

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -1,42 +1,66 @@
 import tkinter
 import pandas as pd
 import random
-import time
+
 BACKGROUND_COLOR = "#B1DDC6"
 FRONT_LANGUAGE_FONT = ('Arial', 40, 'italic')
 FRONT_WORD_FONT = ('Arial', 60, 'bold')
-french_word = ''
-english_word = ''
+FRENCH_WORD = ''
+ENGLISH_WORD = ''
+data_dictionary_item = None
+
 # ------------- Window ------------------------------------
 window = tkinter.Tk()
 window.title('Flashy')
 window.minsize(width=900, height=626)
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
 # ------------- Data retrieval functionality --------------
-df = pd.read_csv('./data/french_words.csv')
-data_dictionary = df.to_dict(orient='records')
+try:
+    df = pd.read_csv('./words_to_learn.csv')
+except FileNotFoundError:
+    df = pd.read_csv('./data/french_words.csv')
+    data_dictionary_list = df.to_dict(orient='records')
+else:
+    data_dictionary_list = df.to_dict(orient='records')
+
+# ------------- Right button, update the data functionality --------------------
+
+
+def remove_correct_words():
+    try:
+        del data_dictionary_list[data_dictionary_list.index(data_dictionary_item)]
+        new_df = pd.DataFrame(data_dictionary_list)
+        new_df.to_csv('./words_to_learn.csv', index=False)
+        show_new_french_word_and_flip_card()
+    except ValueError:
+        show_new_french_word_and_flip_card()
 
 # ------------- New word functionality --------------------
 
+
 def show_new_french_word_and_flip_card():
 
-    data_dictionary_item = random.choice(data_dictionary)
-    global french_word
-    global english_word
+    global data_dictionary_item
+    global FRENCH_WORD
+    global ENGLISH_WORD
+    data_dictionary_item = random.choice(data_dictionary_list)
     canvas.config(bg=BACKGROUND_COLOR)
-    french_word = data_dictionary_item['French']
-    english_word = data_dictionary_item['English']
+    FRENCH_WORD = data_dictionary_item['French']
+    ENGLISH_WORD = data_dictionary_item['English']
     canvas.itemconfig(canvas_image, image=card_front_image)
-    canvas.itemconfig(actual_word, text=french_word, fill='black')
+    canvas.itemconfig(actual_word, text=FRENCH_WORD, fill='black')
     canvas.itemconfig(actual_language, text='French', fill='black')
     window.after(ms=3000, func=flip_card)
 
 # ------------ Flip card functionality --------------------
 
+
 def flip_card():
+
     canvas.itemconfig(canvas_image, image=card_back_image)
     canvas.itemconfig(actual_language, text='English', fill='white')
-    canvas.itemconfig(actual_word, text=english_word, fill='white')
+    canvas.itemconfig(actual_word, text=ENGLISH_WORD, fill='white')
 
 # ------------- Canvas ------------------------------------
 
@@ -49,8 +73,10 @@ actual_language = canvas.create_text(400, 150, text='Ready?', font=FRONT_LANGUAG
 actual_word = canvas.create_text(400, 263, text='Goodluck!', font=FRONT_WORD_FONT)
 
 # ------------- Buttons -----------------------------------
+
 right_button_image = tkinter.PhotoImage(file='./images/right.png')
-right_button = tkinter.Button(image=right_button_image, highlightthickness=0, bg=BACKGROUND_COLOR, command=show_new_french_word_and_flip_card)
+right_button = tkinter.Button(image=right_button_image, highlightthickness=0,
+                              bg=BACKGROUND_COLOR, command=remove_correct_words)
 right_button.grid(row=2, column=2)
 wrong_button_image = tkinter.PhotoImage(file='./images/wrong.png')
 wrong_button = tkinter.Button(image=wrong_button_image, highlightthickness=0, command=show_new_french_word_and_flip_card)
